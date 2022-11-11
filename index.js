655d6bebc53ea6cd5a7bd4c3adb3582e15798929
tauhid880 <tauhid880@gmail.com>
2022-11-11 17:11:30 +0600

    get reviews data

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("legalHub").collection("services");
+    const reviewCollection = client.db("legalHub").collection("reviews");
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -47,6 +48,13 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
+    });
+    // get all reviews
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
     });
   } finally {
   }
