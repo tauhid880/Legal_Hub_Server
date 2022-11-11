74d448a6bdcdeab5937562da715f0e7296ae9f52
tauhid880 <tauhid880@gmail.com>
2022-11-11 10:23:16 +0600

    home page conditional data loaded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -7,6 +8,34 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// User - legalHub
+// Password - prQTvaRD85PPKw1D
+
+// mongoDB connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ezb7aqf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// Load 3 services
+async function run() {
+  try {
+    const serviceCollection = client.db("legalHub").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      // const count = await serviceCollection.estimatedDocumentCount();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 // For test purpose
 app.get("/", (req, res) => {
